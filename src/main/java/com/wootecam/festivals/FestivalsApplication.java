3219abac55ae9d93034ac25a096eae121be782ea
minnim1010 <pms1010123@daum.net>
2024-08-09 17:24:38 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/wootecam/festivals/FestivalsApplication.java
@@ -0,0 +1,13 @@
+package com.wootecam.festivals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalsApplication.class, args);
+	}
+
+}
