d1dfca295ef9da67befc1bf9c95c42b7a960c766
KyuWon  Kim <kyuwon0718@gmail.com>
2024-08-12 21:16:07 +0900

    feat: MemberCreateDto 를 Member 엔티티로 변환하는 메소드 추가
    Jpa Entity 로 변환하기 위한 toEntity 메소드 작성

--- src/main/java/com/wootecam/festivals/domain/member/dto/MemberCreateDto.java
+++ src/main/java/com/wootecam/festivals/domain/member/dto/MemberCreateDto.java
@@ -2,10 +2,10 @@ package com.wootecam.festivals.domain.member.dto;
 
 import com.wootecam.festivals.domain.member.entity.Member;
 
-public record MemberCreateDto(String name, String email, String profileImg) {
+public record MemberCreateDto(String memberName, String email, String profileImg) {
     public Member toEntity() {
         return Member.builder()
-                .name(this.name)
+                .memberName(this.memberName)
                 .email(this.email)
                 .profileImg(this.profileImg)
                 .build();
