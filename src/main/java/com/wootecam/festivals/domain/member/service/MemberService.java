b059d21370a5b0e576d1065cedc17b7994b94f69
KyuWon  Kim <kyuwon0718@gmail.com>
2024-08-12 18:28:30 +0900

    feat: MemberService 인터페이스 생성
    Member 도메인의 비즈니스 로직에 대한 인터페이스

---
+++ src/main/java/com/wootecam/festivals/domain/member/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.wootecam.festivals.domain.member.service;
+
+import com.wootecam.festivals.domain.member.entity.Member;
+
+public interface MemberService {
+
+    Member createMember(MemberCreateDto dto);
+
+    Member getMember(Long id);
+
+    void deleteMember(Long id);
+}
