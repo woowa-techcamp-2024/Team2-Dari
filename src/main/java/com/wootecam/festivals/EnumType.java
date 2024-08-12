b300158159a588b91a93643ff672599e705b0e97
minnim1010 <pms1010123@daum.net>
2024-08-12 16:40:11 +0900

    docs: EnumType 인터페이스에 Javadoc 주석 추가
    Spring Rest Docs에 enum을 나타내기 위해 사용해야 하는 타입임을 명시함

--- src/main/java/com/wootecam/festivals/EnumType.java
+++ src/main/java/com/wootecam/festivals/EnumType.java
@@ -1,5 +1,9 @@
 package com.wootecam.festivals;
 
+/**
+ * Spring Data JPA에서 EnumType을 나타내기 위한 인터페이스
+ * Enum 타입은 getName()과 getDescription()을 구현해야 한다.
+ */
 public interface EnumType {
 
     String getName();
