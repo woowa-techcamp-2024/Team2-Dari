c11a49096645c654e6724d953c60973cd28a5292
minnim1010 <pms1010123@daum.net>
2024-08-13 17:14:22 +0900

    refactor: 스프링 통합 테스트에 필요한 공통 어노테이션 공통화
    - 스프링 통합 테스트에 필요한 공통 어노테이션인 @SpringBootTest와 @ActiveProfile을 가지는 클래스 SpringBootTestConfig 정의
    - 스프링 통합 테스트가 SpringBootTestConfig를 상속 받도록 수정

--- src/test/java/com/wootecam/festivals/domain/organization/service/OrganizationServiceTest.java
+++ src/test/java/com/wootecam/festivals/domain/organization/service/OrganizationServiceTest.java
@@ -6,18 +6,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import com.wootecam.festivals.domain.member.repository.MemberRepository;
 import com.wootecam.festivals.domain.organization.dto.OrganizationCreateDto;
 import com.wootecam.festivals.domain.organization.repository.OrganizationRepository;
+import com.wootecam.festivals.utils.SpringBootTestConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
 @DisplayName("OrganizationService 테스트")
-class OrganizationServiceTest {
+class OrganizationServiceTest extends SpringBootTestConfig {
 
     private final MemberRepository memberRepository;
     private final OrganizationRepository organizationRepository;
