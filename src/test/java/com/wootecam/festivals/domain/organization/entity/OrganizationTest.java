c6c2a188f7dea238aec69582af70247fab57dc25
minnim1010 <pms1010123@daum.net>
2024-08-13 17:26:54 +0900

    test: OrganizationTest 필수 필드 기반 생성 테스트 케이스 추가
    - Organization 필수 필드로 생성하는 테스트 케이스 추가
    - DCI 패턴에 적합하게 수정

--- src/test/java/com/wootecam/festivals/domain/organization/entity/OrganizationTest.java
+++ src/test/java/com/wootecam/festivals/domain/organization/entity/OrganizationTest.java
@@ -10,91 +10,100 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Organization 테스트")
 class OrganizationTest {
 
     @Nested
     @DisplayName("Organization 생성 시")
     class create_organization {
 
-        @DisplayName("이름이 1자 이하라면 예외가 발생한다")
-        @Test
-        void it_returns_exception_name_length_is_less_then_1() {
-            // Given
-            String name = "";
-            String profileImg = "profile.jpg";
-            String detail = "This is a valid detail.";
-
-            // When Then
-            assertThrows(IllegalArgumentException.class, () -> {
-                Organization.builder()
-                        .name(name)
-                        .profileImg(profileImg)
-                        .detail(detail)
-                        .build();
-            });
-        }
-
-        @DisplayName("이름이 20자 초과라면 예외가 발생한다")
-        @Test
-        void it_returns_exception_name_length_is_more_then_20() {
-            // Given
-            String name = "a".repeat(21);
-            String profileImg = "profile.jpg";
-            String detail = "This is a valid detail.";
-
-            // When Then
-            assertThrows(IllegalArgumentException.class, () -> {
-                Organization.builder()
-                        .name(name)
-                        .profileImg(profileImg)
-                        .detail(detail)
-                        .build();
-            });
-        }
-
-        @DisplayName("설명이 200자 초과라면 예외가 발생한다")
-        @Test
-        void it_returns_exception_detail_length_is_more_then_200() {
-            // Given
-            String name = "name";
-            String profileImg = "profile.jpg";
-            String detail = "a".repeat(201);
-
-            // When Then
-            assertThrows(IllegalArgumentException.class, () -> {
-                Organization.builder()
-                        .name(name)
-                        .profileImg(profileImg)
-                        .detail(detail)
-                        .build();
-            });
-        }
-
         @Nested
-        @DisplayName("유효한 이름, 프로필 이미지, 설명이 주어지면")
-        class when_valid_name_profileImg_detail_is_given {
+        @DisplayName("이름, 프로필 이미지, 설명이 주어지면")
+        class when_name_profileImg_detail_is_given {
+
+            String name = "ValidName";
+            String profileImg = "profile.jpg";
+            String detail = "This is a valid detail.";
 
             @Test
             @DisplayName("Organization을 생성한다")
             void it_returns_new_organization() {
-                // Given
-                String name = "ValidName";
-                String profileImg = "profile.jpg";
-                String detail = "This is a valid detail.";
-
-                // When
                 Organization organization = Organization.builder()
                         .name(name)
                         .profileImg(profileImg)
                         .detail(detail)
                         .build();
 
-                // Then
                 assertAll(() -> {
                     assertNotNull(organization);
                     assertEquals(name, organization.getName());
                     assertEquals(profileImg, organization.getProfileImg());
                     assertEquals(detail, organization.getDetail());
+                    assertFalse(organization.isDeleted());
+                });
+            }
+
+            @DisplayName("이름이 1자 이하라면 예외가 발생한다")
+            @Test
+            void it_returns_exception_name_length_is_less_then_1() {
+                String name = "";
+
+                assertThrows(IllegalArgumentException.class, () -> {
+                    Organization.builder()
+                            .name(name)
+                            .profileImg(profileImg)
+                            .detail(detail)
+                            .build();
+                });
+            }
+
+            @DisplayName("이름이 20자 초과라면 예외가 발생한다")
+            @Test
+            void it_returns_exception_name_length_is_more_then_20() {
+                String name = "a".repeat(21);
+
+                assertThrows(IllegalArgumentException.class, () -> {
+                    Organization.builder()
+                            .name(name)
+                            .profileImg(profileImg)
+                            .detail(detail)
+                            .build();
+                });
+            }
+
+            @DisplayName("설명이 200자 초과라면 예외가 발생한다")
+            @Test
+            void it_returns_exception_detail_length_is_more_then_200() {
+                String detail = "a".repeat(201);
+
+                assertThrows(IllegalArgumentException.class, () -> {
+                    Organization.builder()
+                            .name(name)
+                            .profileImg(profileImg)
+                            .detail(detail)
+                            .build();
+                });
+            }
+        }
+
+        @Nested
+        @DisplayName("이름이 주어지면")
+        class when_name_is_given {
+
+            String name = "ValidName";
+
+            @Test
+            @DisplayName("Organization을 생성한다")
+            void it_returns_new_organization() {
+                Organization organization = Organization.builder()
+                        .name(name)
+                        .profileImg(null)
+                        .detail(null)
+                        .build();
+
+                assertAll(() -> {
+                    assertNotNull(organization);
+                    assertEquals(name, organization.getName());
                     assertFalse(organization.isDeleted());
                 });
             }
