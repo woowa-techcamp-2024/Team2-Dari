a5247aae39ca9ca259ad72c977b7cc7dab6f0e5e
minnim1010 <pms1010123@daum.net>
2024-08-13 17:53:08 +0900

    feat: OrganizationMember 정의
    - organization과 member의 매핑 테이블 및 조직 내 권한을 관리하는 OrganizationMember 정의

---
+++ src/main/java/com/wootecam/festivals/domain/organization/entity/OrganizationMember.java
@@ -0,0 +1,35 @@
+package com.wootecam.festivals.domain.organization.entity;
+
+import com.wootecam.festivals.global.audit.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class OrganizationMember extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "organization__member_id")
+    private Long id;
+    @Column(nullable = false)
+    private Long memberId;
+    @Column(nullable = false)
+    private Long organizationId;
+    private OrganizationRole role;
+
+    @Builder
+    private OrganizationMember(Long memberId, Long organizationId, OrganizationRole role) {
+        this.memberId = memberId;
+        this.organizationId = organizationId;
+        this.role = role;
+    }
+}
