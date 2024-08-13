ba66cbf28f39118e7b6b8d4debd407672962bc33
bellringstar <bellringstar@gmail.com>
2024-08-13 22:46:07 +0900

    feat: FestivalController getFestival 메서드 작성
    festivalId를 통해 축제 상세정보를 찾아 반환하는 api 작성

--- src/main/java/com/wootecam/festivals/domain/festival/controller/FestivalController.java
+++ src/main/java/com/wootecam/festivals/domain/festival/controller/FestivalController.java
@@ -2,11 +2,14 @@ package com.wootecam.festivals.domain.festival.controller;
 
 import com.wootecam.festivals.domain.festival.dto.FestivalCreateRequest;
 import com.wootecam.festivals.domain.festival.dto.FestivalCreateResponse;
+import com.wootecam.festivals.domain.festival.dto.FestivalDetailResponse;
 import com.wootecam.festivals.domain.festival.service.FestivalService;
 import com.wootecam.festivals.global.api.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +31,9 @@ public class FestivalController {
         return ApiResponse.of(responseDto);
     }
 
-
+    @GetMapping("/{festivalId}")
+    public ApiResponse<FestivalDetailResponse> getFestival(@PathVariable Long festivalId) {
+        FestivalDetailResponse responseDto = festivalService.getFestivalDetail(festivalId);
+        return ApiResponse.of(responseDto);
+    }
 }
