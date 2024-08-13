65db5b28894df7b3d57cb14d47dd4263564c07d0
kimkw <kyuwon0718@gmail.com>
2024-08-14 01:48:32 +0900

    feat: auth 에러코드 추가
    로그인 실패 에러코드가 포함된 auth 도메인 에러코드 구현

---
+++ src/main/java/com/wootecam/festivals/domain/auth/exception/AuthErrorCode.java
@@ -0,0 +1,33 @@
+package com.wootecam.festivals.domain.auth.exception;
+
+import com.wootecam.festivals.global.docs.EnumType;
+import com.wootecam.festivals.global.exception.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum AuthErrorCode implements ErrorCode, EnumType {
+
+    USER_LOGIN_FAILED(HttpStatus.UNAUTHORIZED, "AU-C-0001", "로그인에 실패하였습니다.")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    AuthErrorCode(HttpStatus httpStatus, String code, String message) {
+        this.code = code;
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    @Override
+    public String getName() {
+        return name();
+    }
+
+    @Override
+    public String getDescription() {
+        return httpStatus + ": " + code + " - " + message;
+    }
+}
