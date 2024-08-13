40cca21a7ae572e99fdaf7a3e9f1ac8bc9d92354
minnim1010 <pms1010123@daum.net>
2024-08-13 13:41:18 +0900

    feat: 서버 내부에서 확인한 예외 핸들러 구현
    서버 내부에서 예외 확인한 핸들러 구현

---
+++ src/main/java/com/wootecam/festivals/global/exception/ApiExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.wootecam.festivals.global.exception;
+
+import com.wootecam.festivals.global.api.ApiErrorResponse;
+import com.wootecam.festivals.global.exception.type.ApiException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackages = "com.wootecam.festivals.domain")
+@Slf4j
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<ApiErrorResponse> handleApiException(ApiException exception) {
+        ApiErrorResponse errorResponse = ApiErrorResponse.of(exception.getErrorCode().getCode(),
+                exception.getErrorDescription());
+
+        log.error("{}", errorResponse);
+
+        return ResponseEntity
+                .status(exception.getErrorCode().getHttpStatus())
+                .body(errorResponse);
+    }
+}
