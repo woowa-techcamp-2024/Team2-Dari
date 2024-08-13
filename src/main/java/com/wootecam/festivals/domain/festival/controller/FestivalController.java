15a2d75348aaffb1ed4ab31f12695ea865eb6368
bellringstar <bellringstar@gmail.com>
2024-08-13 17:01:42 +0900

    feat: FestivalController 작성
    Festival을 생성하기 위한 api 메서드 작성
    
    requestDto를 받아 ApiResponse<Long>을 반환해준다. 데이터로는 festival의 pk가 나간다.

---
+++ src/main/java/com/wootecam/festivals/domain/festival/controller/FestivalController.java
@@ -0,0 +1,25 @@
+package com.wootecam.festivals.domain.festival.controller;
+
+import com.wootecam.festivals.domain.festival.dto.FestivalCreateRequestDto;
+import com.wootecam.festivals.domain.festival.service.FestivalService;
+import com.wootecam.festivals.global.api.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/festivals")
+@RequiredArgsConstructor
+public class FestivalController {
+
+    private final FestivalService festivalService;
+
+    @PostMapping
+    public ApiResponse<Long> createFestival(@Valid @RequestBody FestivalCreateRequestDto requestDto) {
+        Long festivalId = festivalService.createFestival(requestDto);
+        return ApiResponse.of(festivalId);
+    }
+}
