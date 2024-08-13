33758178806c5d2651d43590ee58f1ce7532216b
minnim1010 <pms1010123@daum.net>
2024-08-13 17:14:22 +0900

    refactor: 스프링 통합 테스트에 필요한 공통 어노테이션 공통화
    - 스프링 통합 테스트에 필요한 공통 어노테이션인 @SpringBootTest와 @ActiveProfile을 가지는 클래스 SpringBootTestConfig 정의
    - 스프링 통합 테스트가 SpringBootTestConfig를 상속 받도록 수정

--- src/test/java/com/wootecam/festivals/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/wootecam/festivals/domain/member/service/MemberServiceTest.java
@@ -7,17 +7,14 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.wootecam.festivals.domain.member.dto.MemberCreateDto;
 import com.wootecam.festivals.domain.member.repository.MemberRepository;
+import com.wootecam.festivals.utils.SpringBootTestConfig;
 import com.wootecam.festivals.utils.TestDBCleaner;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
-class MemberServiceTest {
+class MemberServiceTest extends SpringBootTestConfig {
 
     @Autowired
     MemberService memberService;
---
+++ src/test/java/com/wootecam/festivals/utils/SpringBootTestConfig.java
@@ -0,0 +1,9 @@
+package com.wootecam.festivals.utils;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class SpringBootTestConfig {
+}
