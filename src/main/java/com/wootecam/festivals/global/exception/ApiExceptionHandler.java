e8a3d2e02c7e3b4f50772c2fc3d07f7ba0425bb4
minnim1010 <pms1010123@daum.net>
2024-08-13 14:21:14 +0900

    feat: exceptionHandler 클래스에 순서 추가
    명시적 순서 확인을 위해 exceptionHandler 클래스에 ExceptionHandler별 순서 추가함

--- src/main/java/com/wootecam/festivals/global/exception/ApiExceptionHandler.java
+++ src/main/java/com/wootecam/festivals/global/exception/ApiExceptionHandler.java
@@ -3,12 +3,15 @@ package com.wootecam.festivals.global.exception;
 import com.wootecam.festivals.global.api.ApiErrorResponse;
 import com.wootecam.festivals.global.exception.type.ApiException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackages = "com.wootecam.festivals.domain")
 @Slf4j
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class ApiExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
--- src/main/java/com/wootecam/festivals/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wootecam/festivals/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.wootecam.festivals.global.exception;
 
 import com.wootecam.festivals.global.api.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackages = "com.wootecam.festivals.domain")
 @Slf4j
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
