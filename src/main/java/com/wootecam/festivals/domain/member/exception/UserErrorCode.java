34dd008916df3e08fcbbc266fec9d834a477fa8f
kimkw <kyuwon0718@gmail.com>
2024-08-13 23:06:37 +0900

    style: UserErrorCode 개행문자 추가
    코드 컨벤션 따르기 위해 클래스 정의 후 한줄 개행

--- src/main/java/com/wootecam/festivals/domain/member/exception/UserErrorCode.java
+++ src/main/java/com/wootecam/festivals/domain/member/exception/UserErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum UserErrorCode implements ErrorCode, EnumType {
+
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U-0001", "사용자를 찾을 수 없습니다."),
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "U-0002", "중복된 이메일입니다."),
     ;
