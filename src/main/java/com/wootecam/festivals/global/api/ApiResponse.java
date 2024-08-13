20505b776e91b9f041c34c45f93dbd37ab594d22
minnim1010 <pms1010123@daum.net>
2024-08-13 12:23:36 +0900

    feat: 공통 응답 객체 필드 수정
    - JSON 형식에 맞도록 공통 응답 객체의 필드 수정함

--- src/main/java/com/wootecam/festivals/global/api/ApiResponse.java
+++ src/main/java/com/wootecam/festivals/global/api/ApiResponse.java
@@ -2,21 +2,18 @@ package com.wootecam.festivals.global.api;
 
 import lombok.Getter;
 import lombok.ToString;
-import org.springframework.http.HttpStatus;
 
 @ToString
 @Getter
 public class ApiResponse<T> {
 
-    private final HttpStatus status;
     private final T data;
 
-    private ApiResponse(HttpStatus status, T data) {
-        this.status = status;
+    private ApiResponse(T data) {
         this.data = data;
     }
 
-    public static <T> ApiResponse<T> of(HttpStatus status, T data) {
-        return new ApiResponse<>(status, data);
+    public static <T> ApiResponse<T> of(T data) {
+        return new ApiResponse<>(data);
     }
 }
