0ddf9388736b0784fb84c5c14d6808fe0a3dfe21
KyuWon  Kim <kyuwon0718@gmail.com>
2024-08-13 18:27:11 +0900

    feat: 회원탈퇴 컨트롤러 코드 작성
    세션에서 memberId 를 가져와서 db 에 접근
    
    member 의 isDeleted=True 로 수정

--- src/main/java/com/wootecam/festivals/domain/member/controller/MemberController.java
+++ src/main/java/com/wootecam/festivals/domain/member/controller/MemberController.java
@@ -2,9 +2,11 @@ package com.wootecam.festivals.domain.member.controller;
 
 import com.wootecam.festivals.domain.member.dto.MemberCreateDto;
 import com.wootecam.festivals.domain.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +23,17 @@ public class MemberController {
     @PostMapping
     public ResponseEntity<Long> signUpMember(@RequestBody MemberCreateDto memberCreateDto) {
         return new ResponseEntity<>(memberService.createMember(memberCreateDto), HttpStatus.CREATED);
+    }
+
+    // 유저 탈퇴
+    /*
+    기능 확장성을 고려하면 회원 탈퇴할 유저 id 를 받아야함
+     */
+    @DeleteMapping
+    public ResponseEntity<Void> revokeMember(HttpSession session) {
+        //TODO 로그인했을 경우 세션에서 가져오는 로직 추가 필요
+        Long memberId = (Long) session.getAttribute("memberId");
+        memberService.revokeMember(memberId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
