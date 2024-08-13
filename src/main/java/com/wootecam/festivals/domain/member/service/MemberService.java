81d094355998d25c17bb52b94271a700ed56e632
KyuWon  Kim <kyuwon0718@gmail.com>
2024-08-13 18:18:16 +0900

    feat: 회원 탈퇴 서비스 로직 구현
    영속성 컨텍스트를 사용하기 위해 @Transactional 사용
    
    isDeleted=True 로 변경하여 flush 하도록 함

--- src/main/java/com/wootecam/festivals/domain/member/service/MemberService.java
+++ src/main/java/com/wootecam/festivals/domain/member/service/MemberService.java
@@ -1,7 +1,11 @@
 package com.wootecam.festivals.domain.member.service;
 
 import com.wootecam.festivals.domain.member.dto.MemberCreateDto;
+import com.wootecam.festivals.domain.member.entity.Member;
+import com.wootecam.festivals.domain.member.exception.UserErrorCode;
 import com.wootecam.festivals.domain.member.repository.MemberRepository;
+import com.wootecam.festivals.global.exception.type.ApiException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,11 +19,19 @@ public class MemberService {
 
         memberRepository.findByEmail(memberCreateDto.email())
                 .ifPresent(member -> {
-                    throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+                    throw new ApiException(UserErrorCode.DUPLICATED_EMAIL);
                 });
 
         return memberRepository
                 .save(memberCreateDto.toEntity())
                 .getId();
+    }
+
+    @Transactional
+    public void revokeMember(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new ApiException(UserErrorCode.USER_NOT_FOUND));
+
+        member.updateStatusDeleted();
     }
 }
