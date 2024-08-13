8ebe4f6263e5516d0e40d88650cf88fe96d05207
minnim1010 <pms1010123@daum.net>
2024-08-13 23:20:15 +0900

    feat: Organization 상세 조회 api 구현
    - OrganizationErrorCode 정의
    - Organization 상세 조회 로직 구현 및 테스트 정의

--- src/main/java/com/wootecam/festivals/domain/organization/controller/OrganizationController.java
+++ src/main/java/com/wootecam/festivals/domain/organization/controller/OrganizationController.java
@@ -2,11 +2,14 @@ package com.wootecam.festivals.domain.organization.controller;
 
 import com.wootecam.festivals.domain.organization.dto.OrganizationCreateDto;
 import com.wootecam.festivals.domain.organization.dto.OrganizationIdDto;
+import com.wootecam.festivals.domain.organization.dto.OrganizationResponse;
 import com.wootecam.festivals.domain.organization.service.OrganizationService;
 import com.wootecam.festivals.global.api.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +30,10 @@ public class OrganizationController {
         Long organizationId = organizationService.createOrganization(organizationCreateDto);
 
         return ApiResponse.of(new OrganizationIdDto(organizationId));
+    }
+
+    @GetMapping("/{organizationId}")
+    public ApiResponse<OrganizationResponse> findOrganization(@PathVariable Long organizationId) {
+        return ApiResponse.of(organizationService.findOrganization(organizationId));
     }
 }
