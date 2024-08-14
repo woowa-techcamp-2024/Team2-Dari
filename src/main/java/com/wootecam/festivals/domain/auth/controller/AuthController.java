7ff77db5a7304683ccf1966ecc11b3c71d97d175
KyuWon  Kim <kyuwon0718@gmail.com>
2024-08-14 10:53:51 +0900

    refactor: AuthController HttpStatus 응답값 명시
    @ResponseStatus 를 통해 디폴트가 OK 여도 명시하기로 합의되어 수정

--- src/main/java/com/wootecam/festivals/domain/auth/controller/AuthController.java
+++ src/main/java/com/wootecam/festivals/domain/auth/controller/AuthController.java
@@ -4,9 +4,11 @@ import com.wootecam.festivals.domain.auth.service.AuthService;
 import com.wootecam.festivals.domain.dto.LoginRequestDto;
 import com.wootecam.festivals.global.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,12 +18,14 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/login")
     public ApiResponse<Void> login(@RequestBody LoginRequestDto dto) {
         authService.login(dto.email());
         return ApiResponse.of(null);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/logout")
     public ApiResponse<Void> logout() {
         authService.logout();
