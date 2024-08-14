8bf6e46f694cb721f1599ae6f53c30ada672d0e4
bellringstar <bellringstar@gmail.com>
2024-08-14 15:26:06 +0900

    refactor: 매개변수 위치 변경
    매개변수 팀 컨벤션에 맞춰 변경

--- src/main/java/com/wootecam/festivals/domain/festival/dto/FestivalDetailResponse.java
+++ src/main/java/com/wootecam/festivals/domain/festival/dto/FestivalDetailResponse.java
@@ -4,14 +4,13 @@ import com.wootecam.festivals.domain.festival.entity.Festival;
 import com.wootecam.festivals.domain.festival.entity.FestivalStatus;
 import java.time.LocalDateTime;
 
-public record FestivalDetailResponse(
-        Long festivalId,
-        Long organizationId,
-        String title,
-        String description,
-        LocalDateTime startTime,
-        LocalDateTime endTime,
-        FestivalStatus festivalStatus
+public record FestivalDetailResponse(Long festivalId,
+                                     Long organizationId,
+                                     String title,
+                                     String description,
+                                     LocalDateTime startTime,
+                                     LocalDateTime endTime,
+                                     FestivalStatus festivalStatus
 ) {
     public static FestivalDetailResponse from(Festival festival) {
         return new FestivalDetailResponse(
