d4194e6b12b9de96b9e556f4c2fcc7e5f2ddb83f
bellringstar <bellringstar@gmail.com>
2024-08-14 15:26:22 +0900

    feat: FestivalCreateResponse 불필요 코드 삭제
    null 체크가 불필요하기 때문에 해당 체크 로직 삭제

--- src/main/java/com/wootecam/festivals/domain/festival/dto/FestivalCreateResponse.java
+++ src/main/java/com/wootecam/festivals/domain/festival/dto/FestivalCreateResponse.java
@@ -1,23 +1,10 @@
 package com.wootecam.festivals.domain.festival.dto;
 
 import com.wootecam.festivals.domain.festival.entity.Festival;
-import com.wootecam.festivals.domain.festival.exception.FestivalErrorCode;
-import com.wootecam.festivals.global.exception.type.ApiException;
 
-public record FestivalCreateResponse(
-        Long festivalId
+public record FestivalCreateResponse(Long festivalId
 ) {
     public static FestivalCreateResponse from(Festival festival) {
-        validateFestival(festival);
         return new FestivalCreateResponse(festival.getId());
-    }
-
-    private static void validateFestival(Festival festival) {
-        if (festival == null) {
-            throw new ApiException(FestivalErrorCode.InvalidFestivalDataException, "Festival 객체가 null입니다.");
-        }
-        if (festival.getId() == null) {
-            throw new ApiException(FestivalErrorCode.InvalidFestivalDataException, "Festival Id가 null입니다.");
-        }
     }
 }
