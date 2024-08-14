8ebe5712e3f6dc965d724ecf4bb496647f77422d
bellringstar <bellringstar@gmail.com>
2024-08-14 15:41:09 +0900

    feat: FestivalCreateResponseTest 테스트 케이스 삭제
    불필요 유효성 검사 삭제에 따른 테스트코드 삭제

--- src/test/java/com/wootecam/festivals/domain/festival/dto/FestivalCreateResponseTest.java
+++ src/test/java/com/wootecam/festivals/domain/festival/dto/FestivalCreateResponseTest.java
@@ -1,12 +1,9 @@
 package com.wootecam.festivals.domain.festival.dto;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.wootecam.festivals.domain.festival.entity.Festival;
-import com.wootecam.festivals.domain.festival.exception.FestivalErrorCode;
 import com.wootecam.festivals.domain.festival.stub.FestivalStub;
-import com.wootecam.festivals.global.exception.type.ApiException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -30,29 +27,6 @@ class FestivalCreateResponseTest {
 
             // Then
             assertThat(responseDto.festivalId()).isEqualTo(expectedId);
-        }
-
-        @Test
-        @DisplayName("null Festival 객체가 주어지면 ApiException을 던진다")
-        void it_throws_apiException_when_given_null_festival() {
-            // When & Then
-            assertThatThrownBy(() -> FestivalCreateResponse.from(null))
-                    .isInstanceOf(ApiException.class)
-                    .hasFieldOrPropertyWithValue("errorCode", FestivalErrorCode.InvalidFestivalDataException)
-                    .hasMessage("Festival 객체가 null입니다.");
-        }
-
-        @Test
-        @DisplayName("ID가 null인 Festival 객체가 주어지면 ApiException을 던진다")
-        void it_throws_apiException_when_given_festival_with_null_id() {
-            // Given
-            Festival festivalWithNullId = FestivalStub.createFestivalWithNullId();
-
-            // When & Then
-            assertThatThrownBy(() -> FestivalCreateResponse.from(festivalWithNullId))
-                    .isInstanceOf(ApiException.class)
-                    .hasFieldOrPropertyWithValue("errorCode", FestivalErrorCode.InvalidFestivalDataException)
-                    .hasMessage("Festival Id가 null입니다.");
         }
     }
 }
