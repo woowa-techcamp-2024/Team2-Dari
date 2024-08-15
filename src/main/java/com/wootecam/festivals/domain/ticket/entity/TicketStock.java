aeae1da687594c3475856d87f972dc407c8b063f
minnim1010 <pms1010123@daum.net>
2024-08-16 06:35:26 +0900

    feat: TicketStock 정의
    - Ticket 재고 엔티티 TicketStock 정의

---
+++ src/main/java/com/wootecam/festivals/domain/ticket/entity/TicketStock.java
@@ -0,0 +1,38 @@
+package com.wootecam.festivals.domain.ticket.entity;
+
+import com.wootecam.festivals.global.audit.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class TicketStock extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ticket_purchase_id")
+    private Long id;
+
+    @Column(name = "ticket_stock", nullable = false)
+    private int remainStock;
+
+    @OneToOne
+    @JoinColumn(name = "ticket_id", nullable = false, updatable = false)
+    private Ticket ticket;
+
+    @Builder
+    private TicketStock(int remainStock, Ticket ticket) {
+        this.remainStock = remainStock;
+        this.ticket = ticket;
+    }
+}
