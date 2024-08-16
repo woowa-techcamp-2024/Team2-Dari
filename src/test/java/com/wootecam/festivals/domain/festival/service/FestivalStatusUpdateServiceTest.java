d7b5fe6b6903e869e88c3c128841d0dc14b87766
HyeonJun0530 <hyjn053@gmail.com>
2024-08-16 16:00:58 +0900

    test: admin 추가에 따른 테스트 수정
    - 테스트 데이터에 admin 추가

--- src/test/java/com/wootecam/festivals/domain/festival/service/FestivalStatusUpdateServiceTest.java
+++ src/test/java/com/wootecam/festivals/domain/festival/service/FestivalStatusUpdateServiceTest.java
@@ -5,7 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.wootecam.festivals.domain.festival.entity.Festival;
 import com.wootecam.festivals.domain.festival.entity.FestivalStatus;
 import com.wootecam.festivals.domain.festival.repository.FestivalRepository;
+import com.wootecam.festivals.domain.member.entity.Member;
+import com.wootecam.festivals.domain.member.repository.MemberRepository;
+import com.wootecam.festivals.utils.TestDBCleaner;
 import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,16 +24,34 @@ class FestivalStatusUpdateServiceTest {
     @Autowired
     private FestivalRepository festivalRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        TestDBCleaner.clear(festivalRepository);
+        TestDBCleaner.clear(memberRepository);
+    }
+
     @Test
     @DisplayName("updateFestivalStatus 메서드 테스트")
     void testUpdateFestivalStatus() {
         // Given
-        Festival festival = Festival.builder()
-                .title("Festival 1")
-                .description("Description 1")
-                .startTime(LocalDateTime.now().plusMinutes(1))
-                .endTime(LocalDateTime.now().plusMinutes(2))
+        Member member = Member.builder()
+                .name("test")
+                .email("test@test.com")
+                .profileImg("")
                 .build();
+        Member savedMember = memberRepository.save(member);
+
+        Festival festival = Festival.builder()
+                .admin(savedMember)
+                .title("페스티벌")
+                .description("페스티벌 설명")
+                .startTime(LocalDateTime.now().plusDays(1))
+                .endTime(LocalDateTime.now().plusDays(2))
+                .build();
+
         Festival savedFestival = festivalRepository.save(festival);
 
         FestivalStatus newStatus = FestivalStatus.COMPLETED;
