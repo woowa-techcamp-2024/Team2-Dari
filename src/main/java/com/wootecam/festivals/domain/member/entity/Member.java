47c83f1322f7b4a7d67c0673b3b387631f3fb7b3
KyuWon  Kim <kyuwon0718@gmail.com>
2024-08-16 11:03:22 +0900

    fix: MemberServiceTest 잘못된 assert 문 수정

--- src/main/java/com/wootecam/festivals/domain/member/entity/Member.java
+++ src/main/java/com/wootecam/festivals/domain/member/entity/Member.java
@@ -11,7 +11,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
