93b6b8b81dc749262432057c2717179906cfb5b8
bellringstar <bellringstar@gmail.com>
2024-08-16 09:51:52 +0900

    feat: 키셋 페이지네이션 응답을 위한 DTO 추가
    - KeySetPageResponse 레코드 클래스 수정
    - Cursor 클래스 추가
    - 구현 방식:
      - KeySetPageResponse: content, cursor, hasNext 필드 포함
      - Cursor: time, id 필드로 구성
      - 레코드 클래스 사용으로 불변성 보장
      - hasNext가 false일 때 cursor를 null로 설정하는 로직 추가

--- src/main/java/com/wootecam/festivals/domain/festival/dto/KeySetPageResponse.java
+++ src/main/java/com/wootecam/festivals/domain/festival/dto/KeySetPageResponse.java
@@ -3,6 +3,12 @@ package com.wootecam.festivals.domain.festival.dto;
 import java.util.List;
 
 public record KeySetPageResponse<T>(List<T> content,
-                                    Long nextCursor,
+                                    Cursor cursor,
                                     boolean hasNext) {
+
+    public KeySetPageResponse {
+        if (!hasNext) {
+            cursor = null;
+        }
+    }
 }
