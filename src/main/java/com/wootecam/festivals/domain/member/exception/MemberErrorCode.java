b41ce2522fd5434d1559c4dc568087092a0d2e13
bellringstar <bellringstar@gmail.com>
2024-08-17 16:56:18 +0900

    refactor: UserErrorCode.java 네이밍 변경
    - 도메인이 Member이기 때문에 MemberErrorCode로 변경

--- src/main/java/com/wootecam/festivals/domain/member/exception/MemberErrorCode.java
+++ src/main/java/com/wootecam/festivals/domain/member/exception/MemberErrorCode.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public enum UserErrorCode implements ErrorCode, EnumType {
+public enum MemberErrorCode implements ErrorCode, EnumType {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U-0001", "사용자를 찾을 수 없습니다."),
     DUPLICATED_EMAIL(HttpStatus.CONFLICT, "U-0002", "중복된 이메일입니다."),
@@ -16,7 +16,7 @@ public enum UserErrorCode implements ErrorCode, EnumType {
     private final String code;
     private final String message;
 
-    UserErrorCode(HttpStatus httpStatus, String code, String message) {
+    MemberErrorCode(HttpStatus httpStatus, String code, String message) {
         this.code = code;
         this.httpStatus = httpStatus;
         this.message = message;
