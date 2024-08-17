7c3e9934656a6c7db56cbfe13ef9dbbe07cfa8b4
kimkw <kyuwon0718@gmail.com>
2024-08-17 23:06:28 +0900

    feat: CheckinController 구현
    - 체크인 api 앤드포인트 추가

---
+++ src/main/java/com/wootecam/festivals/domain/checkin/controller/CheckinController.java
@@ -0,0 +1,30 @@
+package com.wootecam.festivals.domain.checkin.controller;
+
+import com.wootecam.festivals.domain.checkin.service.CheckinService;
+import com.wootecam.festivals.global.api.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/festivals/{festivalId}/tickets/{ticketId}/checkins")
+@RequiredArgsConstructor
+public class CheckinController {
+
+    private final CheckinService checkinService;
+
+    /**
+     * 체크인 정보를 체크인 되었음으로 업데이트 합니다.
+     * @param checkinId 조회할 체크인의 식별자
+     * @return null
+     */
+    @PatchMapping("/{checkinId}")
+    public ApiResponse<Void> updateCheckedIn(@PathVariable Long checkinId) {
+        checkinService.updateCheckedIn(checkinId);
+        return ApiResponse.of(null);
+    }
+}
