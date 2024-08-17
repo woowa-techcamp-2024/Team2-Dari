1179edf968793163ee2911ef8eedc41465f78926
kimkw <kyuwon0718@gmail.com>
2024-08-17 21:00:16 +0900

    feat: CheckinService 체크인 저장 구현
    getReferenceById 를 통해 엔티티 조회를 지연시켜 checkin 저장

--- src/main/java/com/wootecam/festivals/domain/checkin/service/CheckinService.java
+++ src/main/java/com/wootecam/festivals/domain/checkin/service/CheckinService.java
@@ -3,6 +3,12 @@ package com.wootecam.festivals.domain.checkin.service;
 import com.wootecam.festivals.domain.checkin.entity.Checkin;
 import com.wootecam.festivals.domain.checkin.exception.CheckinErrorCode;
 import com.wootecam.festivals.domain.checkin.repository.CheckinRepository;
+import com.wootecam.festivals.domain.festival.entity.Festival;
+import com.wootecam.festivals.domain.festival.repository.FestivalRepository;
+import com.wootecam.festivals.domain.member.entity.Member;
+import com.wootecam.festivals.domain.member.repository.MemberRepository;
+import com.wootecam.festivals.domain.ticket.entity.Ticket;
+import com.wootecam.festivals.domain.ticket.repository.TicketRepository;
 import com.wootecam.festivals.global.exception.type.ApiException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +19,23 @@ import org.springframework.stereotype.Service;
 public class CheckinService {
 
     private final CheckinRepository checkinRepository;
+    private final MemberRepository memberRepository;
+    private final FestivalRepository festivalRepository;
+    private final TicketRepository ticketRepository;
+
+    @Transactional
+    public Long saveCheckin(Long memberId, Long festivalId, Long ticketId) {
+        Member member = memberRepository.getReferenceById(memberId);
+        Festival festival = festivalRepository.getReferenceById(festivalId);
+        Ticket ticket = ticketRepository.getReferenceById(ticketId);
+
+        return checkinRepository.save(Checkin.builder()
+                .member(member)
+                .festival(festival)
+                .ticket(ticket)
+                .build())
+                .getId();
+    }
 
     @Transactional
     public void updateCheckedIn(Long memberId, Long festivalId, Long ticketId) {
