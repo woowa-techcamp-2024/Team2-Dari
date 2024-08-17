92d28693085ea142bd00880aff1f536a6a230c01
bellringstar <bellringstar@gmail.com>
2024-08-17 17:53:33 +0900

    refactor: 불필요 주석 삭제

--- src/main/java/com/wootecam/festivals/FestivalsApplication.java
+++ src/main/java/com/wootecam/festivals/FestivalsApplication.java
@@ -11,5 +11,4 @@ public class FestivalsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FestivalsApplication.class, args);
 	}
-	// TODO: LocalDateTime 사용시 시, 분 까지만 처리하도록 변경 예정. 축제 생성시 인증 확인으로 변경
 }
