7fabf9f58a813f05381844b63647ca753a9fdf91
kimkw <kyuwon0718@gmail.com>
2024-08-19 00:31:01 +0900

    fix: 티켓 구매 시각 TimeSerializer 적용

--- src/main/java/com/wootecam/festivals/domain/my/dto/MyPurchasedTicketResponse.java
+++ src/main/java/com/wootecam/festivals/domain/my/dto/MyPurchasedTicketResponse.java
@@ -1,12 +1,15 @@
 package com.wootecam.festivals.domain.my.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.wootecam.festivals.domain.festival.dto.FestivalResponse;
 import com.wootecam.festivals.domain.purchase.entity.PurchaseStatus;
 import com.wootecam.festivals.domain.ticket.dto.TicketWithoutStockResponse;
+import com.wootecam.festivals.global.utils.CustomLocalDateTimeSerializer;
 import java.time.LocalDateTime;
 
 public record MyPurchasedTicketResponse(
         Long purchaseId,
+        @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
         LocalDateTime purchaseTime,
         PurchaseStatus purchaseStatus,
         TicketWithoutStockResponse ticket,
