8324c1f6d118d6c9da3af463eea4682ab5ea4586
bellringstar <bellringstar@gmail.com>
2024-08-18 12:22:10 +0900

    feat: Cursor response 변경
    - 시:분 까지만 리스폰스가 나가도록 변경

--- src/main/java/com/wootecam/festivals/domain/festival/dto/Cursor.java
+++ src/main/java/com/wootecam/festivals/domain/festival/dto/Cursor.java
@@ -1,6 +1,9 @@
 package com.wootecam.festivals.domain.festival.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.wootecam.festivals.global.utils.CustomLocalDateTimeSerializer;
 import java.time.LocalDateTime;
 
-public record Cursor(LocalDateTime time, Long id) {
+public record Cursor(@JsonSerialize(using = CustomLocalDateTimeSerializer.class) LocalDateTime time,
+                     Long id) {
 }
