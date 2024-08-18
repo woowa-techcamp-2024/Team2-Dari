15e9fb04e2ecbe52a0c166221fcd0e10ae084822
minnim1010 <pms1010123@daum.net>
2024-08-18 17:34:13 +0900

    feat: 내가 주최한 축제 목록 응답의 시작 시각 객체 포맷팅

--- src/main/java/com/wootecam/festivals/domain/my/dto/MyFestivalCursor.java
+++ src/main/java/com/wootecam/festivals/domain/my/dto/MyFestivalCursor.java
@@ -1,6 +1,9 @@
 package com.wootecam.festivals.domain.my.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.wootecam.festivals.global.utils.CustomLocalDateTimeSerializer;
 import java.time.LocalDateTime;
 
-public record MyFestivalCursor(LocalDateTime startTime, Long id) {
+public record MyFestivalCursor(@JsonSerialize(using = CustomLocalDateTimeSerializer.class) LocalDateTime startTime,
+                               Long id) {
 }
--- src/main/java/com/wootecam/festivals/domain/my/dto/MyFestivalResponse.java
+++ src/main/java/com/wootecam/festivals/domain/my/dto/MyFestivalResponse.java
@@ -1,6 +1,9 @@
 package com.wootecam.festivals.domain.my.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.wootecam.festivals.global.utils.CustomLocalDateTimeSerializer;
 import java.time.LocalDateTime;
 
-public record MyFestivalResponse(Long festivalId, String title, LocalDateTime startTime) {
+public record MyFestivalResponse(Long festivalId, String title, String festivalImg,
+                                 @JsonSerialize(using = CustomLocalDateTimeSerializer.class) LocalDateTime startTime) {
 }
