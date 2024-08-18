750897e07ca7239c6368118d56c12a2422b05f76
bellringstar <bellringstar@gmail.com>
2024-08-18 12:26:35 +0900

    feat: FestivalListResponse, FestivalResponse 수정
    - json response에서 분단위 까지만 나오도록 변경

--- src/main/java/com/wootecam/festivals/domain/festival/dto/FestivalListResponse.java
+++ src/main/java/com/wootecam/festivals/domain/festival/dto/FestivalListResponse.java
@@ -1,14 +1,18 @@
 package com.wootecam.festivals.domain.festival.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.wootecam.festivals.domain.festival.entity.Festival;
 import com.wootecam.festivals.domain.festival.entity.FestivalProgressStatus;
 import com.wootecam.festivals.domain.festival.entity.FestivalPublicationStatus;
+import com.wootecam.festivals.global.utils.CustomLocalDateTimeSerializer;
 import java.time.LocalDateTime;
 
 public record FestivalListResponse(Long festivalId,
                                    String title,
                                    String festivalImg,
+                                   @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
                                    LocalDateTime startTime,
+                                   @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
                                    LocalDateTime endTime,
                                    FestivalPublicationStatus festivalPublicationStatus,
                                    FestivalProgressStatus festivalProgressStatus,
