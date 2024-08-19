a7fff3194555662899412a0d053652dd6dcfce9f
HyeonJun0530 <hyjn053@gmail.com>
2024-08-19 17:03:34 +0900

    feat: 결제 Mock 기능 구현
    - 결제 기능 Mock으로 구현

---
+++ backend/src/main/java/com/wootecam/festivals/domain/purchase/service/PaymentService.java
@@ -0,0 +1,19 @@
+package com.wootecam.festivals.domain.purchase.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PaymentService {
+
+    public void pay(Long memberId, Long ticketId) {
+        log.debug("결제 요청 - 회원 ID: {}, 티켓 ID: {}", memberId, ticketId);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            return;
+        }
+        log.debug("결제 완료 - 회원 ID: {}, 티켓 ID: {}", memberId, ticketId);
+    }
+}
