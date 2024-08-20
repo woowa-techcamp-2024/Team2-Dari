d4e7ab5913aefa20eaf255ad21240419a13585c7
HyeonJun0530 <hyjn053@gmail.com>
2024-08-20 10:12:27 +0900

    feat: PaymentService @Async로 비동기 처리

--- backend/src/main/java/com/wootecam/festivals/domain/purchase/service/PaymentService.java
+++ backend/src/main/java/com/wootecam/festivals/domain/purchase/service/PaymentService.java
@@ -1,12 +1,14 @@
 package com.wootecam.festivals.domain.purchase.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class PaymentService {
 
+    @Async
     public void pay(Long memberId, Long ticketId) {
         log.debug("결제 요청 - 회원 ID: {}, 티켓 ID: {}", memberId, ticketId);
         try {
