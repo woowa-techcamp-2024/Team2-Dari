b384977b65ef47706cf36fae5b56341a4d0d78ed
minnim1010 <pms1010123@daum.net>
2024-08-21 10:30:44 +0900

    feat: 1분마다 티켓 구매 시각 만료 시 티켓 구매 권한 삭제하는 로직 구현 및 테스트 작성

--- backend/src/main/java/com/wootecam/festivals/FestivalsApplication.java
+++ backend/src/main/java/com/wootecam/festivals/FestivalsApplication.java
@@ -4,9 +4,11 @@ import com.wootecam.festivals.global.config.CloudConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(CloudConfiguration.class)
+@EnableScheduling
 public class FestivalsApplication {
 
 	public static void main(String[] args) {
