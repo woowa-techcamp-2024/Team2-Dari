fe23a1674c60d47cf741c8c0bba7f460692aa6d5
minnim1010 <pms1010123@daum.net>
2024-08-21 19:56:47 +0900

    tmp1

--- backend/k6-monitoring/scripts/load-test.js
+++ backend/k6-monitoring/scripts/load-test.js
@@ -14,8 +14,8 @@ const threadPoolMetric = new Trend('thread_pool');  // 스레드 풀 상태
 export const options = {
     // 단계별 부하 테스트 설정
     stages: [
-        {duration: '30s', target: 500},  // 1분 동안 500명의 가상 사용자로 증가
-        {duration: '20s', target: 500},  // 3분 동안 500명의 가상 사용자 유지
+        {duration: '30s', target: 50},  // 1분 동안 500명의 가상 사용자로 증가
+        {duration: '20s', target: 50},  // 3분 동안 500명의 가상 사용자 유지
         {duration: '10s', target: 0},    // 1분 동안 0명으로 감소
     ],
     // 성능 임계값 설정
@@ -30,93 +30,42 @@ export const options = {
 // const BASE_URL = 'http://13.125.202.151:8080/api/v1';  // API 기본 URL
 const BASE_URL = 'http://localhost:8080/api/v1';  // API 기본 URL
 // 로그인 함수
-// async function login(email) {
-//     const loginUrl = `${BASE_URL}/auth/login`;
-//     const payload = JSON.stringify({ email: email });
-//     const params = {
-//         headers: { 'Content-Type': 'application/json',
-//             'Cookie': '',},
-//     };
-//     const res = await http.post(loginUrl, payload, params);
-//     const success = check(res, {
-//         'login successful': (r) => r.status === 200,
-//     });
-//
-//     if (!success) {
-//         console.error('Login failed:', res.status, res.body);
-//         return null;
-//     }
-//
-//     // 쿠키 추출 및 세션 쿠키 파싱
-//     const cookieHeaders = res.request.headers['Cookie'];
-//     if (!cookieHeaders || cookieHeaders.length === 0) {
-//         console.error('No Cookie header found');
-//         return null;
-//     }
-//
-//     const cookieHeaderString = cookieHeaders.join('; ');
-//     const sessionCookieMatch = cookieHeaderString.match(/SESSION=[^;]+/);
-//     if (!sessionCookieMatch) {
-//         console.error('No SESSION cookie found');
-//         return null;
-//     }
-//
-//     return sessionCookieMatch[0];
-// }
-
 async function login(email) {
     const loginUrl = `${BASE_URL}/auth/login`;
-    const payload = JSON.stringify({ email: email });
+    const payload = JSON.stringify({email: email});
     const params = {
-        method: 'POST',  // HTTP 메서드를 POST로 설정
-        headers: {
-            'Content-Type': 'application/json',
-            'Cookie': '',  // 쿠키를 비워서 보내도록 명시적으로 설정
-        },
-        body: payload,  // 요청 본문에 JSON 데이터를 포함
+        headers: {'Content-Type': 'application/json'},
     };
 
-    try {
-        const res = await fetch(loginUrl, params);
-        const success = check(res, {
-            'login successful': (r) => r.status === 200,
-        });
-        console.log(success)
+    // 쿠키 저장소를 가져오기
+    const jar = http.cookieJar();
+    jar.clear('http://localhost:8080');
 
-        if (!success) {
-            console.error('Login failed:', res.status, res.body);
-            return null;
-        }
+    const res = await http.post(loginUrl, payload, params);
+    const success = check(res, {
+        'login successful': (r) => r.status === 200,
+    });
 
-        if (success) {
-            console.log('Login successful');
-            const cookieHeaders = res.request.headers['Cookie'];
-            if (!cookieHeaders || cookieHeaders.length === 0) {
-                console.error('No Cookie header found');
-                return null;
-            }
-
-            const cookieHeaderString = cookieHeaders.join('; ');
-            const sessionCookieMatch = cookieHeaderString.match(/SESSION=[^;]+/);
-            if (!sessionCookieMatch) {
-                console.error('No SESSION cookie found');
-                return null;
-            }
-
-            return sessionCookieMatch[0];
-        } else {
-            console.error('Login failed:', res.status, await res.text());
-        }
-    } catch (error) {
-        console.error('An error occurred during the login request:', error);
+    if (!success) {
+        console.error('Login failed:', res.status, res.body);
+        return null;
     }
+
+    // 쿠키 추출 및 세션 쿠키 파싱
+    const cookieHeaders = res.headers['Set-Cookie'];
+    if (!cookieHeaders || cookieHeaders.length === 0) {
+        console.error('No Cookie header found');
+        return null;
+    }
+
+    return cookieHeaders;
 }
 
 // 페스티벌 목록 조회
 function getFestivalList(sessionCookie, cursor, pageSize) {
     const url = `${BASE_URL}/festivals?${cursor ? `time=${cursor.time}&id=${cursor.id}&` : ''}pageSize=${pageSize}`;
     const response = http.get(url, {
-        headers: { 'Cookie': sessionCookie }
+        headers: {'Cookie': sessionCookie}
     });
 
     const checkRes = check(response, {
@@ -145,7 +94,7 @@ function getFestivalList(sessionCookie, cursor, pageSize) {
 // 페스티벌 상세 조회
 function getFestivalDetail(festivalId, sessionCookie) {
     const response = http.get(`${BASE_URL}/festivals/${festivalId}`, {
-        headers: { 'Cookie': sessionCookie }
+        headers: {'Cookie': sessionCookie}
     });
 
     const checkRes = check(response, {
@@ -174,7 +123,7 @@ function getFestivalDetail(festivalId, sessionCookie) {
 // 티켓 목록 조회
 function getTicketList(festivalId, sessionCookie) {
     const response = http.get(`${BASE_URL}/festivals/${festivalId}/tickets`, {
-        headers: { 'Cookie': sessionCookie }
+        headers: {'Cookie': sessionCookie}
     });
 
     const checkRes = check(response, {
@@ -237,7 +186,7 @@ function checkPurchasable(festivalId, ticketId, sessionCookie) {
 // 구매할 티켓 조회
 function getTicketInfo(festivalId, ticketId, sessionCookie) {
     const response = http.get(`${BASE_URL}/festivals/${festivalId}/tickets/${ticketId}/purchase`, {
-        headers: { 'Cookie': sessionCookie }
+        headers: {'Cookie': sessionCookie}
     });
 
     const checkRes = check(response, {
@@ -300,7 +249,7 @@ export async function setup() {
     console.log('Starting setup...');
 
     const totalUsers = 100000;
-    const usersToLogin = 100;
+    const usersToLogin = 500;
     const totalFestivals = 1000;
     const ticketsPerFestival = 3;
 
@@ -311,30 +260,35 @@ export async function setup() {
         const email = `user${userIndex}@example.com`;
         const sessionCookie = await login(email);
         if (sessionCookie) {
-            loggedInUsers.push({ email, sessionCookie });
+            loggedInUsers.push({email, sessionCookie});
         }
     }
 
     console.log(`Logged in ${loggedInUsers.length} users`);
 
     // 축제id 및 티켓id 데이터 생성
-    const festivals = Array.from({ length: totalFestivals }, (_, i) => {
+    const festivals = Array.from({length: totalFestivals}, (_, i) => {
         const festivalId = i + 1;
-        const ticketIds = Array.from({ length: ticketsPerFestival }, (_, j) => (festivalId - 1) * ticketsPerFestival + j + 1);
-        return { festivalId, ticketIds };
+        const ticketIds = Array.from({length: ticketsPerFestival}, (_, j) => (festivalId - 1) * ticketsPerFestival + j + 1);
+        return {festivalId, ticketIds};
     });
 
-    return { loggedInUsers, festivals };
+    return {loggedInUsers, festivals};
 }
 
 // 메인 테스트 함수
+let userIdx = 1;
 export default function (data) {
     group('User Flow', function () {
         const rand = Math.random();
 
         // console.log(data.loggedInUsers)
         // console.log(Math.floor(rand * data.loggedInUsers.length))
-        let user = data.loggedInUsers[Math.floor(rand * data.loggedInUsers.length)];
+        ++userIdx;
+        if (userIdx >= data.loggedInUsers.length) {
+            userIdx = 1;
+        }
+        let user = data.loggedInUsers[userIdx];
         let festivalData = data.festivals[Math.floor(Math.random() * data.festivals.length)];
         let festivalId = festivalData.festivalId;
         let ticketId = festivalData.ticketIds[Math.floor(Math.random() * festivalData.ticketIds.length)];
@@ -361,7 +315,7 @@ export default function (data) {
             group('Purchase Ticket', function () {
                 const sessionInfo = checkPurchasable(festivalId, ticketId, user.sessionCookie);
                 sleep(0.5);  // 각 반복 사이에 0.5초 대기
-                // console.log(sessionInfo)
+                console.log(sessionInfo)
                 if (sessionInfo != null) {
                     const ticketInfo = getTicketInfo(sessionInfo.festivalId, sessionInfo.ticketId, sessionInfo.sessionCookie);
                     sleep(0.5);  // 각 반복 사이에 0.5초 대기
