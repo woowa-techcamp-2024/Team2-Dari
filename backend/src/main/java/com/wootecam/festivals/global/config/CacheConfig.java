ddf7e4f46b38f4a593ad232faac83a36bc1b4e0a
KyuWon  Kim <kyuwon0718@gmail.com>
2024-08-21 14:17:01 +0900

    refactor: spring-cache 1분 단위로 무효화

--- backend/src/main/java/com/wootecam/festivals/global/config/CacheConfig.java
+++ backend/src/main/java/com/wootecam/festivals/global/config/CacheConfig.java
@@ -14,14 +14,14 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager("festival");
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("festival", "festivalsFirstPage");
         cacheManager.setCaffeine(caffeineCacheBuilder());
         return cacheManager;
     }
 
     Caffeine<Object, Object> caffeineCacheBuilder() {
         return Caffeine.newBuilder()
-                .expireAfterWrite(1, TimeUnit.HOURS)
+                .expireAfterWrite(1, TimeUnit.MINUTES)
                 .maximumSize(100);
     }
 }
