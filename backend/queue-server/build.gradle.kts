3a16300f22113a707aa16bd4864d938b5d8f388c
KyuWon  Kim <kyuwon0718@gmail.com>
2024-08-21 15:52:32 +0900

    feat: 멀티 모듈 구성

---
+++ backend/queue-server/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "com.wootecam"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
