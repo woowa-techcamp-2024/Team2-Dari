8080e093f26ad256e58ecba3f767512f1af084ac
bellringstar <bellringstar@gmail.com>
2024-08-21 15:52:21 +0900

    feat: InMemoryQueue 구현을 위한 인터페이스 작성
    - InMemoryQueue의 동작을 정의

---
+++ backend/src/main/java/com/wootecam/festivals/global/queue/CustomQueue.java
@@ -0,0 +1,11 @@
+package com.wootecam.festivals.global.queue;
+
+import java.util.List;
+
+public interface CustomQueue<T> {
+    void offer(T item);
+    T poll();
+    boolean isEmpty();
+    int size();
+    List<T> pollBatch(int batchSize);
+}
