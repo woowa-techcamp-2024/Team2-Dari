3db46cbb0c43b20561eee7ed75e152d7f7735f02
HyeonJun0530 <hyjn053@gmail.com>
2024-08-22 17:00:40 +0900

    refactor: 세션 확인에서 memberRepository 제외
    - 세션에서 값을 가져와 null 체킹만 하도록 변경
    - 세션이니까 가져와서 서비스에서 FindBy해서 어차피 검증하니까 세션만 있으면 Authentication에 넣어주고 여기서 세션에서 가져온 memberId만 사용해서 실제 member 조회는 각 서비스 조회해서 사용하는 것이 좋겠다 !

--- backend/api-server/src/main/java/com/wootecam/festivals/global/auth/Authentication.java
+++ backend/api-server/src/main/java/com/wootecam/festivals/global/auth/Authentication.java
@@ -2,9 +2,9 @@ package com.wootecam.festivals.global.auth;
 
 import com.wootecam.festivals.domain.member.entity.Member;
 
-public record Authentication(Long memberId, String name, String email) {
+public record Authentication(Long memberId) {
 
     public static Authentication from(Member member) {
-        return new Authentication(member.getId(), member.getName(), member.getEmail());
+        return new Authentication(member.getId());
     }
 }
