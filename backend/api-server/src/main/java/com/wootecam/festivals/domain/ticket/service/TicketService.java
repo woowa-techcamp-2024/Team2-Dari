c05f4928f33cb83d9666f48403494320e334af5f
HyeonJun0530 <hyjn053@gmail.com>
2024-08-22 16:55:42 +0900

    refactor: 필요없는 검증 로직 삭제
    - 페스티벌 조회 후 검증하는 로직 삭제

--- backend/api-server/src/main/java/com/wootecam/festivals/domain/ticket/service/TicketService.java
+++ backend/api-server/src/main/java/com/wootecam/festivals/domain/ticket/service/TicketService.java
@@ -12,6 +12,7 @@ import com.wootecam.festivals.domain.ticket.repository.TicketRepository;
 import com.wootecam.festivals.domain.ticket.repository.TicketStockRepository;
 import com.wootecam.festivals.global.exception.type.ApiException;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -40,7 +41,7 @@ public class TicketService {
     public TicketIdResponse createTicket(Long festivalId, TicketCreateRequest request) {
         log.debug("티켓 생성 요청 - 축제 ID: {}", festivalId);
 
-        Festival festival = festivalRepository.findById(festivalId)
+        Festival festival = Optional.of(festivalRepository.getReferenceById(festivalId))
                 .orElseThrow(() -> {
                     log.warn("축제를 찾을 수 없음 - 축제 ID: {}", festivalId);
                     return new ApiException(FestivalErrorCode.FESTIVAL_NOT_FOUND);
@@ -66,16 +67,11 @@ public class TicketService {
      */
     public TicketListResponse getTickets(Long festivalId) {
         log.debug("티켓 목록 조회 요청 - 축제 ID: {}", festivalId);
-        Festival festival = festivalRepository.findById(festivalId)
-                .orElseThrow(() -> {
-                    log.warn("축제를 찾을 수 없음 - 축제 ID: {}", festivalId);
-                    return new ApiException(FestivalErrorCode.FESTIVAL_NOT_FOUND);
-                });
 
         List<TicketResponse> ticketsByFestivalIdWithRemainStock =
-                ticketRepository.findTicketsByFestivalIdWithRemainStock(festival.getId());
+                ticketRepository.findTicketsByFestivalIdWithRemainStock(festivalId);
         log.debug("티켓 목록: {}", ticketsByFestivalIdWithRemainStock);
 
-        return new TicketListResponse(festival.getId(), ticketsByFestivalIdWithRemainStock);
+        return new TicketListResponse(festivalId, ticketsByFestivalIdWithRemainStock);
     }
 }
