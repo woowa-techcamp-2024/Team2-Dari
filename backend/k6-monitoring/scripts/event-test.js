c63d563f22a04e1618bc11e74ffa0ac54f88f2e8
minnim1010 <pms1010123@daum.net>
2024-08-22 12:36:35 +0900

    chore: 한 티켓을 동시 구매하는 시나리오 스크립트 작성

--- backend/k6-monitoring/scripts/event-test.js
+++ backend/k6-monitoring/scripts/event-test.js
@@ -1,36 +1,20 @@
 import http from 'k6/http';
 import {check, group, sleep} from 'k6';
-import {Rate, Trend} from 'k6/metrics';
 import {randomIntBetween} from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 import exec from 'k6/execution';
 
-// 커스텀 메트릭 정의
-const failureRate = new Rate('failed_requests');  // 실패한 요청 비율
-const responseTime = new Trend('response_time');  // 응답 시간
-const connectionPoolMetric = new Trend('connection_pool');  // 연결 풀 상태
-const threadPoolMetric = new Trend('thread_pool');  // 스레드 풀 상태
+const MAX_USER = 500
+const BASE_ORIGIN = 'http://13.125.202.151:8080';
+const BASE_URL = BASE_ORIGIN + '/api/v1';
 
-const max_user = 5000
 // 테스트 구성 옵션
 export const options = {
     // 단계별 부하 테스트 설정
+    vus: MAX_USER,
+    iterations: MAX_USER,
 
-    stages: [
-        {duration: '30s', target: max_user},
-        {duration: '20s', target: max_user / 2},
-        {duration: '10s', target: 0},
-    ],
-    // 성능 임계값 설정
-    thresholds: {
-        http_req_duration: ['p(95)<100'],  // 95%의 요청이 100ms 이내에 완료되어야 함
-        failed_requests: ['rate<0.1'],     // 실패율이 10% 미만이어야 함
-        response_time: ['p(95) < 200'],    // 95%의 응답 시간이 200ms 미만이어야 함
-    },
     setupTimeout: '3m',  // 셋업 단계의 최대 실행 시간
 };
-
-const BASE_ORIGIN = 'http://localhost:8080';
-const BASE_URL = BASE_ORIGIN + '/api/v1';
 
 // 로그인 함수
 async function login(email) {
@@ -45,9 +29,7 @@ async function login(email) {
     jar.clear(BASE_ORIGIN);
 
     const res = await http.post(loginUrl, payload, params);
-    const success = check(res, {
-        'login successful': (r) => r.status === 200,
-    });
+    const success = res.status === 200;
 
     if (!success) {
         console.error('Login failed:', res.status, res.body);
@@ -88,9 +70,6 @@ function getFestivalList(sessionCookie, cursor, pageSize) {
         console.error('Get festival list failed:', response.status, response.body);
     }
 
-    failureRate.add(!checkRes);
-    responseTime.add(response.timings.duration);
-
     return checkRes ? JSON.parse(response.body).data : null;
 }
 
@@ -116,9 +95,6 @@ function getFestivalDetail(festivalId, sessionCookie) {
     if (!checkRes) {
         console.error('Get festival detail failed:', response.status, response.body);
     }
-
-    failureRate.add(!checkRes);
-    responseTime.add(response.timings.duration);
 
     return checkRes ? JSON.parse(response.body).data : null;
 }
@@ -146,14 +122,11 @@ function getTicketList(festivalId, sessionCookie) {
         console.error('Get ticket list failed:', response.status, response.body);
     }
 
-    failureRate.add(!checkRes);
-    responseTime.add(response.timings.duration);
-
     return checkRes ? JSON.parse(response.body).data.tickets : null;
 }
 
 // 티켓 결제 가능 여부 조회
-function checkPurchasable(festivalId, ticketId, sessionCookie) {
+function checkPurchasable(festivalId, ticketId, sessionCookie, email) {
     const response = http.get(`${BASE_URL}/festivals/${festivalId}/tickets/${ticketId}/purchase/check`, {
         headers: {'Cookie': sessionCookie}
     });
@@ -177,11 +150,8 @@ function checkPurchasable(festivalId, ticketId, sessionCookie) {
     });
 
     if (!checkRes) {
-        console.error('Check Purchasable failed:', response.status, response.body);
+        console.error('Check Purchasable failed:', email, response.status, response.body);
     }
-
-    failureRate.add(!checkRes);
-    responseTime.add(response.timings.duration);
 
     return checkRes ? result : null;
 }
@@ -209,9 +179,6 @@ function getTicketInfo(festivalId, ticketId, sessionCookie) {
         console.error('Get ticket info failed:', response.status, response.body);
         console.log(sessionCookie)
     }
-
-    failureRate.add(!checkRes);
-    responseTime.add(response.timings.duration);
 
     return checkRes ? JSON.parse(response.body).data : null;
 }
@@ -243,9 +210,6 @@ function purchaseTicket(festivalId, ticketId, sessionCookie) {
         console.log(sessionCookie)
     }
 
-    failureRate.add(!checkRes);
-    responseTime.add(response.timings.duration);
-
     return checkRes ? JSON.parse(response.body).data : null;
 }
 
@@ -254,9 +218,7 @@ export async function setup() {
     console.log('Starting setup...');
 
     const totalUsers = 100000;
-    const usersToLogin = max_user;
-    const totalFestivals = 1000;
-    const ticketsPerFestival = 3;
+    const usersToLogin = MAX_USER;
 
     // 사용자 로그인 및 세션 쿠키 획득
     const loggedInUsers = [];
@@ -264,81 +226,64 @@ export async function setup() {
         const userIndex = randomIntBetween(1, totalUsers);
         const email = `user${userIndex}@example.com`;
         const sessionCookie = await login(email);
-        let lastUsed = new Date().getTime() - 1000000;
         if (sessionCookie) {
-            loggedInUsers.push({email, sessionCookie, lastUsed});
+            loggedInUsers.push({email, sessionCookie});
         }
     }
 
     console.log(`Logged in ${loggedInUsers.length} users`);
 
-    // 축제id 및 티켓id 데이터 생성
-    const festivals = Array.from({length: totalFestivals}, (_, i) => {
-        const festivalId = i + 1;
-        const ticketIds = Array.from({length: ticketsPerFestival}, (_, j) => (festivalId - 1) * ticketsPerFestival + j + 1);
-        return {festivalId, ticketIds};
-    });
-
-    return {loggedInUsers, festivals};
+    return {loggedInUsers};
 }
 
 // 메인 테스트 함수
+const festivalId = 1;
+const ticketId = 1;
 export default function (data) {
     group('User Flow', function () {
-        const userId = __VU;
-        const user = data.loggedInUsers[userId % data.loggedInUsers.length];
-        const festivalData = data.festivals[Math.floor(Math.random() * data.festivals.length)];
-        const festivalId = festivalData.festivalId;
-        const ticketId = festivalData.ticketIds[Math.floor(Math.random() * festivalData.ticketIds.length)];
+        const uniqueUserId = exec.scenario.iterationInTest;
+        const user = data.loggedInUsers[uniqueUserId];
 
-        const rand = Math.random();
-        // API 호출 비율에 따른 시나리오 실행
-        if (rand < 0.4) {
-            group('Festival List', function () {
-                const festivalListData = getFestivalList(user.sessionCookie, null, 10);
-                if (festivalListData && festivalListData.content && festivalListData.content.length > 0) {
-                    const selectedFestival = festivalListData.content[Math.floor(Math.random() * festivalListData.content.length)];
-                    getFestivalDetail(selectedFestival.festivalId, user.sessionCookie);
-                }
-            });
-        } else if (rand < 0.65) {
-            group('Festival Detail', function () {
-                getFestivalDetail(festivalId, user.sessionCookie);
-            });
-        } else if (rand < 0.9) {
-            group('Ticket List', function () {
-                getTicketList(festivalId, user.sessionCookie);
-            })
-        } else {
-            group('Purchase Ticket', function () {
-                const sessionInfo = checkPurchasable(festivalId, ticketId, user.sessionCookie);
-                sleep(0.5);
-                if (sessionInfo != null) {
-                    const ticketInfo = getTicketInfo(sessionInfo.festivalId, sessionInfo.ticketId, sessionInfo.sessionCookie);
+        const festivalListData = getFestivalList(user.sessionCookie, null, 10);
+        sleep(0.5);
+        if (festivalListData && festivalListData.content && festivalListData.content.length > 0) {
+            const selectedFestival = festivalListData.content[Math.floor(Math.random() * festivalListData.content.length)];
+            let festivalDetailResult = getFestivalDetail(selectedFestival.festivalId, user.sessionCookie);
+            let ticketListResult = getTicketList(festivalId, user.sessionCookie);
+
+            while (true) {
+                if (festivalDetailResult !== null && ticketListResult !== null) {
+                    const sessionInfo = checkPurchasable(festivalId, ticketId, user.sessionCookie, user.email);
                     sleep(0.5);
-                    if (ticketInfo) {
-                        purchaseTicket(sessionInfo.festivalId, sessionInfo.ticketId, sessionInfo.sessionCookie);
+                    if (sessionInfo != null) {
+                        const ticketInfo = getTicketInfo(sessionInfo.festivalId, sessionInfo.ticketId, sessionInfo.sessionCookie);
+                        sleep(0.5);
+                        if (ticketInfo) {
+                            let purchaseTicketResult = purchaseTicket(sessionInfo.festivalId, sessionInfo.ticketId, sessionInfo.sessionCookie);
+                            if (purchaseTicketResult !== null) {
+                                console.log('구매 성공 ', user.email)
+                                return;
+                            }
+                            console.log('구매 실패 ', purchaseTicketResult)
+                        }
                     }
                 }
-            });
+                sleep(0.5);
+            }
         }
+
+        sleep(10000);
     });
-
-    // 시스템 메트릭 기록
-    connectionPoolMetric.add(exec.instance.vusActive);
-    threadPoolMetric.add(exec.instance.iterationsCompleted);
-
-    sleep(0.5);  // 각 반복 사이에 0.5초 대기
 }
 
 /* 사용법:
-k6 run load-test.js
+k6 run event-test.js
 
 grafana + influxdb 사용시
-k6 run --out influxdb=http://localhost:8086/k6 load-test.js
+k6 run --out influxdb=http://localhost:8086/k6 event-test.js
 
 옵션:
-- 가상 사용자 수 변경: k6 run --vus 100 load-test.js
-- 실행 시간 변경: k6 run --duration 30s load-test.js
-- 결과를 파일로 저장: k6 run load-test.js --out json=results.json
+- 가상 사용자 수 변경: k6 run --vus 100 event-test.js
+- 실행 시간 변경: k6 run --duration 30s event-test.js
+- 결과를 파일로 저장: k6 run event-test.js --out json=results.json
 */
