95838c86e124fe30cceab74dae96acfd8374a6fd
bellringstar <bellringstar@gmail.com>
2024-08-23 13:27:50 +0900

    feat: 재고 차감 위치 변경
    - 결제 로직에서 재고를 차감하지 않고 페이지에 접근하기 전 결제 페이지 접근 권한을 주며 재고를 차감
    - 결제에 실패시 재고를 원복해주도록 처

--- backend/api-server/src/main/java/com/wootecam/festivals/domain/purchase/service/PurchaseFacadeService.java
+++ backend/api-server/src/main/java/com/wootecam/festivals/domain/purchase/service/PurchaseFacadeService.java
@@ -1,15 +1,11 @@
 package com.wootecam.festivals.domain.purchase.service;
 
-import com.wootecam.festivals.domain.checkin.dto.CheckinIdResponse;
 import com.wootecam.festivals.domain.checkin.service.CheckinService;
 import com.wootecam.festivals.domain.payment.excpetion.PaymentErrorCode;
 import com.wootecam.festivals.domain.payment.service.PaymentService;
 import com.wootecam.festivals.domain.payment.service.PaymentService.PaymentStatus;
-import com.wootecam.festivals.domain.purchase.dto.PurchaseIdResponse;
-import com.wootecam.festivals.domain.purchase.dto.PurchaseTicketResponse;
 import com.wootecam.festivals.domain.purchase.exception.PurchaseErrorCode;
 import com.wootecam.festivals.domain.ticket.dto.CachedTicketInfo;
-import com.wootecam.festivals.domain.ticket.exception.TicketErrorCode;
 import com.wootecam.festivals.domain.ticket.repository.TicketStockRepository;
 import com.wootecam.festivals.domain.ticket.service.TicketCacheService;
 import com.wootecam.festivals.global.exception.type.ApiException;
@@ -42,30 +38,9 @@ public class PurchaseFacadeService {
 
     private final Map<String, PurchaseData> pendingPurchases = new ConcurrentHashMap<>();
 
-    // 삭제 예정
-    @Deprecated
-    @Transactional
-    public PurchaseTicketResponse purchaseTicket(Long memberId, Long festivalId, Long ticketId) {
-        log.debug("티켓 구매 요청 - 축제 ID: {}, 티켓 ID: {}, 회원 ID: {}", festivalId, ticketId, memberId);
-        PurchaseIdResponse purchaseResponse = purchaseService.createPurchase(ticketId, memberId, LocalDateTime.now());
-        log.debug("티켓 구매 완료 - 구매 ID: {}", purchaseResponse.purchaseId());
-
-        paymentService.pay(memberId, ticketId);
-
-        log.debug("체크인 정보 생성 요청 - 티켓 ID: {}, 회원 ID: {}", ticketId, memberId);
-        CheckinIdResponse checkinResponse = checkinService.createPendingCheckin(memberId, ticketId);
-        log.debug("체크인 정보 생성 완료 - 체크인 ID {}", checkinResponse);
-
-        return new PurchaseTicketResponse(purchaseResponse.purchaseId(), checkinResponse.checkinId());
-    }
-
     @Transactional
     public String processPurchase(PurchaseData purchaseData) {
         validatePurchase(purchaseData);
-        int updated = ticketStockRepository.decreaseStockAtomically(purchaseData.ticketId());
-        if (updated <= 0) {
-            throw new ApiException(TicketErrorCode.TICKET_STOCK_EMPTY);
-        }
 
         String paymentId = paymentService.initiatePayment(purchaseData.memberId(), purchaseData.ticketId());
         pendingPurchases.put(paymentId, purchaseData);
@@ -121,7 +96,6 @@ public class PurchaseFacadeService {
     private boolean isTicketAvailableForPurchase(Long ticketId) {
         CachedTicketInfo ticketInfo = ticketCacheService.getTicketInfo(ticketId); // 없다면 내부에서 db조회 후 가져온다.
 
-        // 도메인 로직 아닌가?
         LocalDateTime now = timeProvider.getCurrentTime();
         return now.isAfter(ticketInfo.startSaleTime()) && now.isBefore(ticketInfo.endSaleTime());
     }
