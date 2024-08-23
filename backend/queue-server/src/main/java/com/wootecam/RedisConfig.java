4a5183e75311e62052689df5e423b82ba8e60cbc
KyuWon  Kim <kyuwon0718@gmail.com>
2024-08-23 17:47:30 +0900

    style: 불필요한 주석 삭제

--- backend/queue-server/src/main/java/com/wootecam/RedisConfig.java
+++ backend/queue-server/src/main/java/com/wootecam/RedisConfig.java
@@ -1,12 +1,9 @@
 package com.wootecam;
 
-import lombok.Builder.Default;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
