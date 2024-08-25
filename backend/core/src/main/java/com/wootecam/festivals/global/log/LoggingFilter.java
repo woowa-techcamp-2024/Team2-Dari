f1ed208a5bf8e15d7ebe0c297754a27aeed78b81
minnim1010 <pms1010123@daum.net>
2024-08-25 21:07:30 +0900

    refactor: 필요하지 않은 로깅 로직 삭제

--- backend/core/src/main/java/com/wootecam/festivals/global/log/LoggingFilter.java
+++ backend/core/src/main/java/com/wootecam/festivals/global/log/LoggingFilter.java
@@ -10,13 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.ContentCachingResponseWrapper;
 
 @Component
 @Slf4j
 public class LoggingFilter extends OncePerRequestFilter {
 
-    private static void logResponse(RequestWrapper request, ContentCachingResponseWrapper response, long elapsedTime) {
+    private static void logResponse(HttpServletRequest request, HttpServletResponse response, long elapsedTime) {
         log.info(
                 "[{}] {} | Status: {} | Elapsed Time: {}ms",
                 String.format("%-5s", request.getMethod()),
@@ -39,22 +38,12 @@ public class LoggingFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         MDC.put("traceId", UUID.randomUUID().toString());
-        if (isAsyncDispatch(request)) {
-            filterChain.doFilter(request, response);
-        } else {
-            doFilterWrapped(new RequestWrapper(request), new ResponseWrapper(response), filterChain);
-        }
-        MDC.clear();
-    }
-
-    protected void doFilterWrapped(RequestWrapper request, ContentCachingResponseWrapper response,
-                                   FilterChain filterChain) throws ServletException, IOException {
         long start = System.currentTimeMillis();
         try {
             filterChain.doFilter(request, response);
         } finally {
             logResponse(request, response, System.currentTimeMillis() - start);
-            response.copyBodyToResponse();
         }
+        MDC.clear();
     }
 }
