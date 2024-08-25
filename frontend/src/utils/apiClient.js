4a779079530d90127f57fc718cf57719e128a933
bellringstar <118344103+bellringstar@users.noreply.github.com>
2024-08-25 14:27:41 +0900

    feat#132/frontend 화면 구성 (#133)
    * feat: Headless UI 적용
    
    * feat: Headless UI 적용
    
    * feat: 축제 리스트, 디테일, 티켓 구매 페이지 구성
    
    * feat: logout api 인증 체크 제외
    
    * feat: AuthContext 수정
    
    * feat: 티켓 구매 확인 페이지 구성
    
    * feat: 축제 생성페이지, 회원가입 페이지
    
    * feat: MyPage 구성
    
    - MyPage 화면 구성
    - 화면 내에서 탭으로 이동하도록 처리
    
    * feat: FestivalManagement 화면 구성
    
    - 마이페이지 축제목록에서 진입할 수 있는 관리페이지
    
    * feat: 무한스크롤 고도화
    
    - 중복으로 나오는 것을 방지
    - 로그아웃시 메인화면으로 리다이렉트
    
    * feat: textarea
    
    * fix: interceptor 제외 경로 수정
    
    - 로그아웃이 제외되던 문제 수정
    
    ---------
    
    Co-authored-by: bellringstar <bellringstar@gmail.com>

---
+++ frontend/src/utils/apiClient.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'http://localhost:8080/api/v1',
+    withCredentials: true,
+});
+
+apiClient.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem('isAuthenticated');
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default apiClient;
