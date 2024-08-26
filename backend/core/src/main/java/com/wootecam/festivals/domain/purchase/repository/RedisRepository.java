d423dbad477f09917a950fef304701e15dc54b7d
minnim1010 <pms1010123@daum.net>
2024-08-26 09:41:14 +0900

    feat: 티켓 재고 수량 key값 수정

--- backend/core/src/main/java/com/wootecam/festivals/domain/purchase/repository/RedisRepository.java
+++ backend/core/src/main/java/com/wootecam/festivals/domain/purchase/repository/RedisRepository.java
@@ -8,7 +8,7 @@ public abstract class RedisRepository {
 
     public final String TICKETS_PREFIX = "tickets:";
     public final String WAITINGS_PREFIX = "waitings:";
-    public final String TICKET_STOCK_COUNT_PREFIX = "ticketStocks:count:";
+    public final String TICKET_STOCK_COUNT_PREFIX = "ticketStocks:count";
     public final String PURCHASED_MEMBERS_PREFIX = "purchasedMembers:";
     public final String CURRENTLY_PAYING_MEMBERS_PREFIX = "currentlyPayingMembers:";
 
