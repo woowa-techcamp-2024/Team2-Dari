dab84b0f37299de23723b970c9165241d552210f
kimkw <kyuwon0718@gmail.com>
2024-08-27 21:23:12 +0900

    fix: TicketRepository#findUpcomingAndOngoingSaleTickets 로직 수정
    - upcoming 만 구현되어있어 ongoing 를 or 조건으로 추가
    - 불필요한 메소드 삭제

--- backend/api-server/src/main/java/com/wootecam/festivals/domain/ticket/repository/TicketRepository.java
+++ backend/api-server/src/main/java/com/wootecam/festivals/domain/ticket/repository/TicketRepository.java
@@ -35,20 +35,8 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
                 t.startSaleTime, t.endSaleTime, t.refundEndTime, t.createdAt, t.updatedAt
             ) 
             FROM Ticket t 
-            WHERE t.startSaleTime >= :now 
+            WHERE t.startSaleTime >= :now OR (t.startSaleTime <= :now AND t.endSaleTime >= :now)
             AND t.isDeleted = false
     """)
     List<TicketResponse> findUpcomingAndOngoingSaleTickets(LocalDateTime now);
-
-    @Query("""
-            SELECT new com.wootecam.festivals.domain.ticket.dto.TicketResponse(
-                t.id, t.name, t.detail, t.price, t.quantity, 
-                (SELECT count(ts.id) FROM TicketStock ts WHERE ts.ticket.id = t.id AND ts.memberId IS NULL),
-                t.startSaleTime, t.endSaleTime, t.refundEndTime, t.createdAt, t.updatedAt
-            ) 
-            FROM Ticket t 
-            WHERE t.startSaleTime <= :now AND t.endSaleTime >= :now
-            AND t.isDeleted = false
-    """)
-    List<TicketResponse> findSaleOngoingTicketsWithRemainStock(LocalDateTime now);
 }
