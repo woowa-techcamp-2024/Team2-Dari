db8637639b3b951b102845f445d1f8f48dbaa620
kimkw <kyuwon0718@gmail.com>
2024-08-27 21:12:33 +0900

    feat: 티켓 재고 count 업데이트 정적 -> 동적 스케줄링 변경
    - 판매기간이 여러 날에 걸친 경우 1분마다 스케줄링을 모두 갖기엔 부하가 되기 때문에 하루에 한번씩 스케줄링 진행

--- backend/api-server/src/main/java/com/wootecam/festivals/domain/ticket/service/TicketScheduleService.java
+++ backend/api-server/src/main/java/com/wootecam/festivals/domain/ticket/service/TicketScheduleService.java
@@ -4,7 +4,9 @@ import com.wootecam.festivals.domain.purchase.repository.TicketInfoRedisReposito
 import com.wootecam.festivals.domain.purchase.repository.TicketStockRedisRepository;
 import com.wootecam.festivals.domain.ticket.dto.TicketResponse;
 import com.wootecam.festivals.domain.ticket.repository.TicketRepository;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,8 +28,7 @@ public class TicketScheduleService {
     private final ThreadPoolTaskScheduler taskScheduler;
 
     /**
-     * 판매 진행중이거나 앞으로 판매될 티켓의 메타 정보를 Redis에 저장
-     * - Ticket 의 startSaleTime, endSaleTime
+     * 판매 진행중이거나 앞으로 판매될 티켓의 메타 정보를 Redis에 저장 - Ticket 의 startSaleTime, endSaleTime
      */
     @EventListener(ContextRefreshedEvent.class)
     public void scheduleRedisTicketInfoUpdate() {
@@ -53,31 +54,43 @@ public class TicketScheduleService {
     }
 
     /**
-     * 현재 판매중인 티켓에 대해 Redis에 저장된 티켓 남은 재고 count 를 주기적으로 업데이트 (1분 주기)
-     * - TicketStock 의 남은 재고 count
+     * 판매 진행중이거나 앞으로 판매될 티켓에 대해 Redis에 저장된 티켓 남은 재고 count 를 주기적으로 업데이트 (1분 주기로 하루치 스케줄링) - TicketStock 의 남은 재고 count
      */
-//    @Scheduled(fixedDelay = 60000, initialDelay = 0)
-    @Scheduled(fixedDelay = 60000, initialDelay = 0)
+    @EventListener(ContextRefreshedEvent.class)
+    @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행
     public void scheduleRedisTicketRemainStockUpdate() {
-        log.debug("Redis에 남은 티켓 재고 업데이트 시작 시각 {}", LocalDateTime.now());
+        log.debug("Redis에 남은 티켓 재고 업데이트 스케줄링 시작 시각 {}", LocalDateTime.now());
 
-        // 현재 판매 진행 중인 티켓 조회
-        List<TicketResponse> tickets = ticketRepository.findSaleOngoingTicketsWithRemainStock(LocalDateTime.now());
-        log.debug("현재 판매 진행 중인 티켓 조회 - 티켓 개수: {}", tickets.size());
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime endOfDay = now.withHour(23).withMinute(59).withSecond(59);
+
+        List<TicketResponse> tickets = ticketRepository.findUpcomingAndOngoingSaleTickets(now);
 
         for (TicketResponse ticket : tickets) {
             if (ticket.remainStock() < 0) {
-                log.error("티켓 재고가 음수입니다. 티켓 ID: {}, 남은 재고: {}", ticket.id(), ticket.remainStock());
+                log.error("티켓 재고가 음수이기 때문에 스케줄링이 진행되지 않습니다. 티켓 ID: {}, 남은 재고: {}", ticket.id(), ticket.remainStock());
                 continue;
             }
 
-            // Redis 에 남은 티켓 재고 업데이트 (tickets:ticketId:ticketStocks:count)
-            ticketStockRedisRepository.setTicketStockCount(ticket.id(), ticket.remainStock());
+            // 현재 판매 중인 티켓은 즉시 업데이트
+            if (ticket.startSaleTime().isBefore(now) && ticket.endSaleTime().isAfter(now)) {
+                updateRedisTicketStockCount(ticket);
+                log.debug("Redis에 저장된 티켓 남은 재고 count 즉시 업데이트 완료 - 티켓 ID: {}, 남은 재고: {}", ticket.id(),
+                        ticket.remainStock());
+            }
 
-            log.debug("Redis에 저장된 티켓 남은 재고 count 업데이트 - 티켓 ID: {}, 남은 재고: {}", ticket.id(), ticket.remainStock());
+            LocalDateTime ticketStartTime = ticket.startSaleTime().isAfter(now) ? ticket.startSaleTime() : now;
+            LocalDateTime ticketEndTime = ticket.endSaleTime().isBefore(endOfDay) ? ticket.endSaleTime() : endOfDay;
+
+            CronTrigger trigger = createDailyUpdateRedisTicketStockCountCronTrigger(ticketStartTime, ticketEndTime);
+            taskScheduler.schedule(() -> updateRedisTicketStockCount(ticket), trigger);
+            log.debug("Redis에 저장된 티켓 남은 재고 count 스케줄링 완료 - 티켓 ID: {}, 시작 시각: {}, 종료 시각: {}",
+                    ticket.id(), ticketStartTime, ticketEndTime);
+
         }
 
-        log.debug("Redis에 남은 티켓 재고 업데이트 종료 Update 된 원소 개수 {}", tickets.size());
+        log.debug("Redis에 남은 티켓 재고 업데이트 스케줄링 종료. 스케줄링된 티켓 개수: {}", tickets.size());
+
     }
 
     private void updateRedisTicketInfo(TicketResponse ticket) {
@@ -85,6 +98,13 @@ public class TicketScheduleService {
         ticketInfoRedisRepository.setTicketInfo(ticket.id(), ticket.startSaleTime(), ticket.endSaleTime());
 
         log.debug("Redis에 저장된 티켓 정보 업데이트 - 티켓 ID: {}, 남은 재고: {}", ticket.id(), ticket.remainStock());
+    }
+
+    private void updateRedisTicketStockCount(TicketResponse ticket) {
+        // Redis 에 남은 티켓 재고 업데이트 (tickets:ticketId:ticketStocks:count)
+        ticketStockRedisRepository.setTicketStockCount(ticket.id(), ticket.remainStock());
+
+        log.debug("Redis에 저장된 티켓 남은 재고 count 업데이트 - 티켓 ID: {}, 남은 재고: {}", ticket.id(), ticket.remainStock());
     }
 
     // 판매 시간 전이라면 판매 10분 전에 스케줄링
@@ -98,6 +118,20 @@ public class TicketScheduleService {
                 scheduledTime.getHour(),
                 scheduledTime.getDayOfMonth(),
                 scheduledTime.getMonthValue());
+
+        return new CronTrigger(cronExpression);
+    }
+
+    /**
+     * 티켓의 판매 시작 시각부터 판매 종료 시각까지 1분마다 스케줄링
+     */
+    private CronTrigger createDailyUpdateRedisTicketStockCountCronTrigger(LocalDateTime start, LocalDateTime end) {
+        String cronExpression = String.format("%d %d-%d %d-%d * * ?",
+                start.getSecond(),
+                start.getMinute(),
+                59, // 매 분 실행
+                start.getHour(),
+                end.getHour());
 
         return new CronTrigger(cronExpression);
     }
