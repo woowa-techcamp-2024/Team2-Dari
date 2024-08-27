ea916cbe5d0ffc2dc8a6c30b82ab9ff3bba56d22
minnim1010 <pms1010123@daum.net>
2024-08-27 20:49:57 +0900

    feat: 헬스 체크 api 구현

---
+++ backend/core/src/main/java/com/wootecam/festivals/domain/purchase/HealthController.java
@@ -0,0 +1,19 @@
+package com.wootecam.festivals.domain.purchase;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/health")
+@RequiredArgsConstructor
+public class HealthController {
+
+    @ResponseStatus(HttpStatus.OK)
+    public void health() {
+    }
+}
