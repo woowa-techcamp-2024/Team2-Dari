f50e92ef4271a4f1a822a3e9510f68d695476613
bellringstar <bellringstar@gmail.com>
2024-08-29 00:21:36 +0900

    feat: 강제 리다이렉트 적용

--- frontend/src/utils/apiClient.js
+++ frontend/src/utils/apiClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://twodari.shop/api/v1',
-    // baseURL: '/api/v1',
+    baseURL: '/api/v1', // 배포
+    // baseURL: 'http://twodari.shop/api/v1',
     // baseURL: 'http://localhost:8080/api/v1',
     withCredentials: true,
 });
