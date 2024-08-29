b7480fa5db7583d695cf1fff492b660e96c62bc9
bellringstar <bellringstar@gmail.com>
2024-08-29 16:15:27 +0900

    feat: 웹 프론트엔드 cors 설정

--- backend/core/src/main/java/com/wootecam/festivals/global/config/WebMvcConfig.java
+++ backend/core/src/main/java/com/wootecam/festivals/global/config/WebMvcConfig.java
@@ -39,7 +39,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://fantastic-bubblegum-58f6ad.netlify.app")
+                .allowedOrigins("http://localhost:3000", "https://twodari.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
