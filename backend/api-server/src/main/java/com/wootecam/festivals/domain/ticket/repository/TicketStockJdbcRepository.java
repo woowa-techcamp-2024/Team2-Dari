1916c9efaf46faa169c0ebfd9ec1700cd3d83fd9
bellringstar <118344103+bellringstar@users.noreply.github.com>
2024-08-30 09:18:49 +0900

    Hotfix#/캐시로 인해 페이지가 나오지 않던 문제 수정, 배치업데이트 버그 수정 (#173)
    * fix: 티켓, 축제 생성 시 캐시 무효화
    
    * fix: 티켓 재고 생성시 created_at, updated_at 추가
    
    * fix: 에러메시지 구체화
    
    ---------
    
    Co-authored-by: bellringstar <bellringstar@gmail.com>

--- backend/api-server/src/main/java/com/wootecam/festivals/domain/ticket/repository/TicketStockJdbcRepository.java
+++ backend/api-server/src/main/java/com/wootecam/festivals/domain/ticket/repository/TicketStockJdbcRepository.java
@@ -3,6 +3,7 @@ package com.wootecam.festivals.domain.ticket.repository;
 import com.wootecam.festivals.domain.ticket.entity.TicketStock;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
@@ -16,13 +17,16 @@ public class TicketStockJdbcRepository {
     private final JdbcTemplate jdbcTemplate;
 
     public void saveTicketStocks(List<TicketStock> ticketStocks) {
-        String sql = "INSERT INTO ticket_stock (ticket_id) VALUES (?)";
+        String sql = "INSERT INTO ticket_stock (ticket_id, created_at, updated_at) VALUES (?, ?, ?)";
+        Timestamp now = new Timestamp(System.currentTimeMillis());
 
         jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
             @Override
             public void setValues(PreparedStatement ps, int index) throws SQLException {
                 TicketStock ticketStock = ticketStocks.get(index);
                 ps.setLong(1, ticketStock.getTicket().getId());
+                ps.setTimestamp(2, now);
+                ps.setTimestamp(3, now);
             }
 
             @Override
